3af0b1b235b65ce2bb608197e65b2b46c175157c
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-01-09 17:24:49 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 import re
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-# import delira
+import delira
 
 # -- Project information -----------------------------------------------------
 
