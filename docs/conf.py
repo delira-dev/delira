d496cd119a5097952dea7bd7610c24289c18a525
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-01-09 20:28:13 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ import re
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 import delira
+import torchvision
 
 # -- Project information -----------------------------------------------------
 
