3814bcd3e2ef18545cabcc36163a979fed45abd9
ORippler <oliver.rippel@gmx.de>
2019-01-16 18:02:57 +0100

    add AbstractTfNetwork

--- delira/models/__init__.py
+++ delira/models/__init__.py
@@ -1,11 +1,11 @@
-from .abstract_network import AbstractNetwork, AbstractPyTorchNetwork
+from .abstract_network import AbstractNetwork, AbstractPyTorchNetwork, \
+    AbstractTfNetwork
 from .classification import VGG3DClassificationNetworkPyTorch, \
     ClassificationNetworkBasePyTorch
 
 from .segmentation import UNet2dPyTorch, UNet3dPyTorch
 
 from .gan import GenerativeAdversarialNetworkBasePyTorch
-
 
 from .classification import __all__ as __all_clf
 from .gan import __all__ as __all_gan
@@ -14,6 +14,7 @@ from .segmentation import __all__ as __all_seg
 __all__ = [
     'AbstractNetwork',
     'AbstractPyTorchNetwork',
+    'AbstractTfNetwork',
     *__all_clf,
     *__all_gan,
     *__all_seg
