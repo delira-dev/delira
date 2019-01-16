f9ebbba57ed9aeb6c63b1508fe8fb9621071936b
ORippler <oliver.rippel@gmx.de>
2019-01-16 18:04:20 +0100

    refactor, add _add_lossses and _add_optims

--- delira/models/classification/__init__.py
+++ delira/models/classification/__init__.py
@@ -2,3 +2,6 @@ import os
 if "torch" in os.environ["DELIRA_BACKEND"]:
     from .classification_network import ClassificationNetworkBasePyTorch
     from .classification_network_3D import VGG3DClassificationNetworkPyTorch
+if "tf" in os.environ["DELIRA_BACKEND"]:
+    from .classification_network_tf import ClassificationNetworkBaseTf
+    from .ResNet18 import ResNet18
