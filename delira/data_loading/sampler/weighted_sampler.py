Bug fix sampler and numerical stability for dummyataset

delira/data_loading/sampler/weighted_sampler.py
@@ -1,5 +1,5 @@
-from ..dataset import AbstractDataset
-from .abstract_sampler import AbstractSampler
+from delira.data_loading.dataset import AbstractDataset
+from delira.data_loading.sampler.abstract_sampler import AbstractSampler
 
 from numpy.random import choice
 import numpy as np
@@ -93,7 +93,7 @@ class WeightedRandomSampler(AbstractSampler):
 
 
 class WeightedPrevalenceRandomSampler(WeightedRandomSampler):
-    def __int__(self, indices):
+    def __init__(self, indices):
         """
         Implements random Per-Class Sampling and ensures uniform sampling
         of all classes
@@ -105,14 +105,14 @@ class WeightedPrevalenceRandomSampler(WeightedRandomSampler):
             data index and the value at a certain index indicates the
              corresponding class
         """
-        weights = np.array(indices)
+        weights = np.array(indices).astype(np.float)
         classes, classes_count = np.unique(indices, return_counts=True)
 
         # compute probabilities
-        classes_count = classes_count / weights.shape[0]
+        target_prob = 1/classes.shape[0]
 
         # generate weight matrix
         for i, c in enumerate(classes):
-            weights[weights == c] = classes_count[i]
+            weights[weights == c] = (target_prob/classes_count[i])
 
         super().__init__(indices, weights=weights)

tests/data_loading/test_sampler.py
@@ -49,7 +49,6 @@ def test_prevalence_random_sampler():
                 assert False, "Label already seen and labels must be unique. \
                                 Batch length: %d" % batch_len
 
-
     assert len(sampler(5)) == 5
 
 
@@ -131,44 +130,27 @@ def test_weighted_sampler():
 
     sampler = WeightedRandomSampler.from_dataset(dset)
 
-    for batch_len in [1, 2, 3]:
+    assert len(sampler(250)) == 250
 
-        equal_batch = sampler(batch_len)
-
-        seen_labels = []
-        for idx in equal_batch:
-            curr_label = dset[idx]["label"]
-
-            if curr_label not in seen_labels:
-                seen_labels.append(curr_label)
-            else:
-                assert False, "Label already seen and labels must be unique. \
-                                Batch length: %d" % batch_len
-
-    assert len(sampler(5)) == 5
+    # checks if labels are all the same (should not happen if random sampled)
+    assert len(set([dset[_idx]["label"] for _idx in sampler(301)])) > 1
 
 
 def test_weighted_prevalence_sampler():
     np.random.seed(1)
-    dset = DummyDataset(600, [0.5, 0.3, 0.2])
+    dset = DummyDataset(2000, [0.5, 0.3, 0.2])
 
     sampler = WeightedPrevalenceRandomSampler.from_dataset(dset)
 
-    for batch_len in [1, 2, 3]:
+    assert len(sampler(250)) == 250
 
-        equal_batch = sampler(batch_len)
-
-        seen_labels = []
-        for idx in equal_batch:
-            curr_label = dset[idx]["label"]
-
-            if curr_label not in seen_labels:
-                seen_labels.append(curr_label)
-            else:
-                assert False, "Label already seen and labels must be unique. \
-                                Batch length: %d" % batch_len
-
-    assert len(sampler(5)) == 5
+    # checks if labels are all the same (should not happen if random sampled)
+    n_draw = 1000
+    label_list = [dset[_idx]["label"] for _idx in sampler(n_draw)]
+    assert len(set(label_list)) > 1
+    assert abs(label_list.count(0)/n_draw - (1 / 3)) < 0.1
+    assert abs(label_list.count(1)/n_draw - (1 / 3)) < 0.1
+    assert abs(label_list.count(2)/n_draw - (1 / 3)) < 0.1
 
 
 if __name__ == '__main__':
@@ -179,3 +161,5 @@ if __name__ == '__main__':
     test_sequential_sampler()
     test_stopping_prevalence_random_sampler()
     test_stopping_prevalence_sequential_sampler()
+    test_weighted_sampler()
+    test_weighted_prevalence_sampler()