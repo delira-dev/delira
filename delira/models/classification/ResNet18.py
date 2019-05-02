b481946ac45ecede8d6388eb9dcc2ec6598b1a71
ORippler <oliver.rippel@gmx.de>
2019-05-02 17:32:01 +0200

    hard-code complete ResNet18

--- delira/models/classification/ResNet18.py
+++ delira/models/classification/ResNet18.py
@@ -18,23 +18,40 @@ class ResNet18(tf.keras.Model):
 
         self.conv2_1 = conv2d(filters=64, strides=1, kernel_size=3,
                               padding='same')
+        self.batchnorm2_1 = batchnorm2d(axis=1)
         self.conv2_2 = conv2d(filters=64, strides=1, kernel_size=3,
                               padding='same')
+        self.batchnorm2_2 = batchnorm2d(axis=1)
 
+        self.identity_3 = conv2d(filters=128, strides=2, kernel_size=1,
+                              padding='same')                              
+        self.batchnorm3 = batchnorm2d(axis=1)
         self.conv3_1 = conv2d(filters=128, strides=2, kernel_size=3,
                               padding='same')
+        self.batchnorm3_1 = batchnorm2d(axis=1)
         self.conv3_2 = conv2d(filters=128, strides=1, kernel_size=3,
                               padding='same')
+        self.batchnorm3_2 = batchnorm2d(axis=1)
 
+        self.identity_4 = conv2d(filters=256, strides=2, kernel_size=1,
+                              padding='same')                              
+        self.batchnorm4 = batchnorm2d(axis=1)
         self.conv4_1 = conv2d(filters=256, strides=2, kernel_size=3,
                               padding='same')
+        self.batchnorm4_1 = batchnorm2d(axis=1)
         self.conv4_2 = conv2d(filters=256, strides=1, kernel_size=3,
                               padding='same')
+        self.batchnorm4_2 = batchnorm2d(axis=1)
 
+        self.identity_5 = conv2d(filters=512, strides=2, kernel_size=1,
+                              padding='same')                              
+        self.batchnorm5 = batchnorm2d(axis=1)
         self.conv5_1 = conv2d(filters=512, strides=2, kernel_size=3,
                               padding='same')
+        self.batchnorm5_1 = batchnorm2d(axis=1)
         self.conv5_2 = conv2d(filters=512, strides=1, kernel_size=3,
                               padding='same')
+        self.batchnorm5_2 = batchnorm2d(axis=1)
 
         self.dense1 = dense(1000)
         self.dense2 = dense(num_classes)
@@ -49,28 +66,43 @@ class ResNet18(tf.keras.Model):
         x = self.relu(x)
         x = self.pool1(x)
 
-        _x = self.conv2_1(x)
-        _x = self.relu(_x)
-        _x = self.conv2_2(_x)
-        x = self.add([x, _x])
+        id = x
+        x = self.conv2_1(x)
+        x = self.batchnorm2_1(x)
+        x = self.relu(x)
+        x = self.conv2_2(x)
+        x = self.batchnorm2_2(x)
+        x = self.add([x, id])
         x = self.relu(x)
 
-        _x = self.conv3_1(x)
-        _x = self.relu(_x)
-        _x = self.conv3_2(_x)
-        x = self.add([x, _x])
+        id = self.identity_3(x)
+        id = self.batchnorm3(id)
+        x = self.conv3_1(x)
+        x = self.batchnorm3_1(x)
+        x = self.relu(x)
+        x = self.conv3_2(x)
+        x = self.batchnorm3_2(x)
+        x = self.add([x, id])
         x = self.relu(x)
 
-        _x = self.conv4_1(x)
-        _x = self.relu(_x)
-        _x = self.conv4_2(_x)
-        x = self.add([x, _x])
+        id = self.identity_4(x)
+        id = self.batchnorm4(id)
+        x = self.conv4_1(x)
+        x = self.batchnorm4_1(x)
+        x = self.relu(x)
+        x = self.conv4_2(x)
+        x = self.batchnorm4_2(x)
+        x = self.add([x, id])
         x = self.relu(x)
 
-        _x = self.conv5_1(x)
-        _x = self.relu(_x)
-        _x = self.conv5_2(_x)
-        x = self.add([x, _x])
+        id = self.identity_5(x)
+        id = self.batchnorm5(id)
+        x = self.conv5_1(x)
+        x = self.batchnorm5_1(x)
+        x = self.relu(x)
+        x = self.conv5_2(x)
+        x = self.batchnorm5_2(x)
+        x = self.add([x, id])
         x = self.relu(x)
 
         x = self.gap(x)
