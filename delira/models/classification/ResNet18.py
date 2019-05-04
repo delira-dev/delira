ba2f571d5c347dba4568b5162fde9a0b93fe4710
ORippler <oliver.rippel@gmx.de>
2019-05-04 18:20:21 +0200

    refactor ResNet18

--- delira/models/classification/ResNet18.py
+++ delira/models/classification/ResNet18.py
@@ -8,105 +8,123 @@ gap2d = tf.keras.layers.GlobalAveragePooling2D
 batchnorm2d = tf.keras.layers.BatchNormalization
 add = tf.keras.layers.Add
 
-class ResNet18(tf.keras.Model):
-    def __init__(self, num_classes=None):
-        super(ResNet18, self).__init__()
-        self.conv1 = conv2d(filters=64, strides=2, kernel_size=7,
-                            padding='same')
-        self.batchnorm1 = batchnorm2d(axis=1)
-        self.pool1 = maxpool2d(pool_size=3, strides=2)
 
-        self.conv2_1 = conv2d(filters=64, strides=1, kernel_size=3,
-                              padding='same')
-        self.batchnorm2_1 = batchnorm2d(axis=1)
-        self.conv2_2 = conv2d(filters=64, strides=1, kernel_size=3,
-                              padding='same')
-        self.batchnorm2_2 = batchnorm2d(axis=1)
+def get_image_format_and_axis():
+    image_format = tf.keras.backend.image_data_format()
+    if image_format == "channels_first":
+        return image_format, 1
+    elif image_format == "channels_last":
+        return image_format, -1
 
-        self.identity_3 = conv2d(filters=128, strides=2, kernel_size=1,
-                              padding='same')                              
-        self.batchnorm3 = batchnorm2d(axis=1)
-        self.conv3_1 = conv2d(filters=128, strides=2, kernel_size=3,
-                              padding='same')
-        self.batchnorm3_1 = batchnorm2d(axis=1)
-        self.conv3_2 = conv2d(filters=128, strides=1, kernel_size=3,
-                              padding='same')
-        self.batchnorm3_2 = batchnorm2d(axis=1)
+_bias = False
 
-        self.identity_4 = conv2d(filters=256, strides=2, kernel_size=1,
-                              padding='same')                              
-        self.batchnorm4 = batchnorm2d(axis=1)
-        self.conv4_1 = conv2d(filters=256, strides=2, kernel_size=3,
-                              padding='same')
-        self.batchnorm4_1 = batchnorm2d(axis=1)
-        self.conv4_2 = conv2d(filters=256, strides=1, kernel_size=3,
-                              padding='same')
-        self.batchnorm4_2 = batchnorm2d(axis=1)
 
-        self.identity_5 = conv2d(filters=512, strides=2, kernel_size=1,
-                              padding='same')                              
-        self.batchnorm5 = batchnorm2d(axis=1)
-        self.conv5_1 = conv2d(filters=512, strides=2, kernel_size=3,
-                              padding='same')
-        self.batchnorm5_1 = batchnorm2d(axis=1)
-        self.conv5_2 = conv2d(filters=512, strides=1, kernel_size=3,
-                              padding='same')
-        self.batchnorm5_2 = batchnorm2d(axis=1)
+class ResBlock(tf.keras.Model):
+    def __init__(self, filters_in: int, filters: int,
+                 strides: tuple, kernel_size: int):
+        super(ResBlock, self).__init__()
 
-        self.dense1 = dense(1000)
-        self.dense2 = dense(num_classes)
-       
-        self.gap = gap2d()
+        _, _axis = get_image_format_and_axis()
+
+        self.identity = None
+        if filters_in != filters:
+            self.identity = conv2d(
+                                filters=filters, strides=strides[0],
+                                kernel_size=1, padding='same', use_bias=_bias)
+            self.bnorm_identity = batchnorm2d(axis=_axis)
+
+        self.conv_1 = conv2d(
+                        filters=filters, strides=strides[0],
+                        kernel_size=kernel_size,
+                        padding='same', use_bias=_bias)
+        self.batchnorm_1 = batchnorm2d(axis=_axis)
+
+        self.conv_2 = conv2d(
+                        filters=filters, strides=strides[1],
+                        kernel_size=kernel_size,
+                        padding='same', use_bias=_bias)
+        self.batchnorm_2 = batchnorm2d(axis=_axis)
+
         self.relu = relu()
         self.add = add()
 
     def call(self, inputs, training=None):
+
+        if self.identity:
+            identity = self.identity(inputs)
+            identity = self.bnorm_identity(identity, training=training)
+        else:
+            identity = inputs
+
+        x = self.conv_1(inputs)
+        x = self.batchnorm_1(x, training=training)
+        x = self.relu(x)
+        x = self.conv_2(x)
+        x = self.batchnorm_2(x, training=training)
+        x = self.add([x, identity])
+        x = self.relu(x)
+
+        return x
+
+
+class ResNet18(tf.keras.Model):
+    def __init__(self, num_classes=None):
+        super(ResNet18, self).__init__()
+
+        _image_format, _axis = get_image_format_and_axis()
+
+        self.conv1 = conv2d(filters=64, strides=2, kernel_size=7,
+                            padding='same', use_bias=_bias)
+        self.batchnorm1 = batchnorm2d(axis=_axis)
+        self.relu = relu()
+        self.pool1 = maxpool2d(pool_size=3, strides=2)
+
+        self.block_2_1 = ResBlock(filters_in=64, filters=64,
+                                  strides=(1, 1), kernel_size=3)
+
+        self.block_2_2 = ResBlock(filters_in=64, filters=64,
+                                  strides=(1, 1), kernel_size=3)
+
+        self.block_3_1 = ResBlock(filters_in=64, filters=128,
+                                  strides=(2, 1), kernel_size=3)
+
+        self.block_3_2 = ResBlock(filters_in=128, filters=128,
+                                  strides=(1, 1), kernel_size=3)
+
+        self.block_4_1 = ResBlock(filters_in=128, filters=256,
+                                  strides=(2, 1), kernel_size=3)
+
+        self.block_4_2 = ResBlock(filters_in=256, filters=256,
+                                  strides=(1, 1), kernel_size=3)
+
+        self.block_5_1 = ResBlock(filters_in=256, filters=512,
+                                  strides=(2, 1), kernel_size=3)
+
+        self.block_5_2 = ResBlock(filters_in=512, filters=512,
+                                  strides=(1, 1), kernel_size=3)
+        self.dense = dense(num_classes)
+        self.gap = gap2d(data_format=_image_format)
+
+    def call(self, inputs, training=None):
+
         x = self.conv1(inputs)
-        x = self.batchnorm1(x)
+        x = self.batchnorm1(x, training=training)
         x = self.relu(x)
         x = self.pool1(x)
 
-        identity = x
-        x = self.conv2_1(x)
-        x = self.batchnorm2_1(x)
-        x = self.relu(x)
-        x = self.conv2_2(x)
-        x = self.batchnorm2_2(x)
-        x = self.add([x, identity])
-        x = self.relu(x)
+        x = self.block_2_1(x)
+        x = self.block_2_2(x)
 
-        identity = self.identity_3(x)
-        identity = self.batchnorm3(identity)
-        x = self.conv3_1(x)
-        x = self.batchnorm3_1(x)
-        x = self.relu(x)
-        x = self.conv3_2(x)
-        x = self.batchnorm3_2(x)
-        x = self.add([x, identity])
-        x = self.relu(x)
+        x = self.block_3_1(x)
+        x = self.block_3_2(x)
 
-        identity = self.identity_4(x)
-        identity = self.batchnorm4(identity)
-        x = self.conv4_1(x)
-        x = self.batchnorm4_1(x)
-        x = self.relu(x)
-        x = self.conv4_2(x)
-        x = self.batchnorm4_2(x)
-        x = self.add([x, identity])
-        x = self.relu(x)
+        x = self.block_4_1(x)
+        x = self.block_4_2(x)
 
-        identity = self.identity_5(x)
-        identity = self.batchnorm5(identity)
-        x = self.conv5_1(x)
-        x = self.batchnorm5_1(x)
-        x = self.relu(x)
-        x = self.conv5_2(x)
-        x = self.batchnorm5_2(x)
-        x = self.add([x, identity])
-        x = self.relu(x)
+        x = self.block_5_1(x)
+        x = self.block_5_2(x)
 
         x = self.gap(x)
-        x = self.dense1(x)
+        x = self.dense(x)
 
-        x = self.dense2(x)
         return x
