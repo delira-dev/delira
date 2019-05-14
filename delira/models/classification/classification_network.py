c1c6540dfd0a496327df5d32e955d3f8c6724e45
Justus Schock <justus.schock@rwth-aachen.de>
2019-05-14 11:10:07 +0200

    Move to new APEX.amp API

--- delira/models/classification/classification_network.py
+++ delira/models/classification/classification_network.py
@@ -10,6 +10,9 @@ if "TORCH" in get_backends():
     from torchvision import models as t_models
     from delira.models.abstract_network import AbstractPyTorchNetwork
 
+    # use loss scaling if installed, otherwise falls back to "normal" backward
+    from ..model_utils import scale_loss
+
     class ClassificationNetworkBasePyTorch(AbstractPyTorchNetwork):
         """
         Implements basic classification with ResNet18
@@ -142,8 +145,9 @@ if "TORCH" in get_backends():
             if optimizers:
                 optimizers['default'].zero_grad()
                 # perform loss scaling via apex if half precision is enabled
-                with optimizers["default"].scale_loss(total_loss) \
-                        as scaled_loss:
+
+                with scale_loss(total_loss,
+                                optimizers["default"]) as scaled_loss:
                     scaled_loss.backward()
                 optimizers['default'].step()
 
