208f5aa61348acdb1422c09543a2e1017d342ade
Justus Schock <justus.schock@rwth-aachen.de>
2019-05-14 12:45:42 +0200

    Add JIT trainer

--- delira/training/pytorch_trainer.py
+++ delira/training/pytorch_trainer.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import typing
 import numpy as np
 from tqdm.auto import tqdm
 import warnings
@@ -17,8 +18,9 @@ if "TORCH" in get_backends():
     import torch
     from .train_utils import convert_torch_tensor_to_npy
     from .train_utils import create_optims_default_pytorch as create_optims_default
-    from ..io.torch import load_checkpoint, save_checkpoint
-    from ..models import AbstractPyTorchNetwork
+    from ..io.torch import load_checkpoint, save_checkpoint, \
+        save_checkpoint_jit, load_checkpoint_jit
+    from ..models import AbstractPyTorchNetwork, AbstractPyTorchJITNetwork
 
     class PyTorchNetworkTrainer(BaseNetworkTrainer):
         """
@@ -62,7 +64,7 @@ if "TORCH" in get_backends():
 
             Parameters
             ----------
-            network : :class:`AbstractTfNetwork`
+            network : :class:`AbstractPyTorchNetwork`
                 the network to train
             save_path : str
                 path to save networks to
@@ -152,7 +154,7 @@ if "TORCH" in get_backends():
                 network, save_path, crits, optimizer_cls, optimizer_params,
                 train_metrics, val_metrics, lr_scheduler_cls,
                 lr_scheduler_params, gpu_ids, save_freq, optim_fn, key_mapping,
-                logging_type, logging_kwargs, fold, callbacks, start_epoch, 
+                logging_type, logging_kwargs, fold, callbacks, start_epoch,
                 metric_keys, convert_batch_to_npy_fn, val_freq)
 
             self._setup(network, optim_fn, optimizer_cls, optimizer_params,
@@ -220,39 +222,7 @@ if "TORCH" in get_backends():
                 v, num_loss=len(self.losses)) for k, v
                 in self.optimizers.items()}
 
-            # Load latest epoch file if available
-            if os.path.isdir(self.save_path):
-                # check all files in directory starting with "checkpoint" and 
-                # not ending with "_best.pth"
-                files = [x for x in os.listdir(self.save_path)
-                         if os.path.isfile(os.path.join(self.save_path, x))
-                         and x.startswith("checkpoint")
-                         and not (x.endswith("_best.pth")
-                                  or x.endswith("_best.pt"))]
-
-                # if list is not empty: load previous state
-                if files:
-
-                    latest_epoch = max([
-                        int(x.rsplit("_", 1)[-1].rsplit(".", 1)[0])
-                        for x in files])
-
-                    latest_state_path = os.path.join(self.save_path,
-                                                     "checkpoint_epoch_%d.pth"
-                                                     % latest_epoch)
-
-                    # if pth file does not exist, load pt file instead
-                    if not os.path.isfile(latest_state_path):
-                        latest_state_path = latest_state_path[:-1]
-
-                    logger.info("Attempting to load state from previous \
-                                training from %s" % latest_state_path)
-                    try:
-                        self.update_state(latest_state_path)
-                    except KeyError:
-                        logger.warn("Previous State could not be loaded, \
-                                    although it exists.Training will be \
-                                    restarted")
+            self.try_resume_training()
 
             if gpu_ids and torch.cuda.is_available():
                 self.use_gpu = True
@@ -285,6 +255,45 @@ if "TORCH" in get_backends():
                 self._prepare_batch, input_device=self.input_device,
                 output_device=self.output_device)
 
+        def try_resume_training(self):
+            """
+            Load the latest checkpoint of a previous training if possible
+
+            """
+            # Load latest epoch file if available
+            if os.path.isdir(self.save_path):
+                # check all files in directory starting with "checkpoint" and
+                # not ending with "_best.pth"
+                files = [x for x in os.listdir(self.save_path)
+                         if os.path.isfile(os.path.join(self.save_path, x))
+                         and x.startswith("checkpoint")
+                         and not (x.endswith("_best.pth")
+                                  or x.endswith("_best.pt"))]
+
+                # if list is not empty: load previous state
+                if files:
+
+                    latest_epoch = max([
+                        int(x.rsplit("_", 1)[-1].rsplit(".", 1)[0])
+                        for x in files])
+
+                    latest_state_path = os.path.join(self.save_path,
+                                                     "checkpoint_epoch_%d.pth"
+                                                     % latest_epoch)
+
+                    # if pth file does not exist, load pt file instead
+                    if not os.path.isfile(latest_state_path):
+                        latest_state_path = latest_state_path[:-1]
+
+                    logger.info("Attempting to load state from previous \
+                                training from %s" % latest_state_path)
+                    try:
+                        self.update_state(latest_state_path)
+                    except KeyError:
+                        logger.warning("Previous State could not be loaded, \
+                                        although it exists.Training will be \
+                                        restarted")
+
         def _at_training_begin(self, *args, **kwargs):
             """
             Defines behaviour at beginning of training
@@ -311,7 +320,7 @@ if "TORCH" in get_backends():
                 best network
 
             """
-            if os.path.isfile(os.path.join(self.save_path, 
+            if os.path.isfile(os.path.join(self.save_path,
                                            'checkpoint_best.pt')):
 
                 # load best model and return it
@@ -374,7 +383,7 @@ if "TORCH" in get_backends():
 
             self.module.train()
 
-            return super()._train_single_epoch(batchgen, epoch, 
+            return super()._train_single_epoch(batchgen, epoch,
                                                verbose=verbose)
 
         def predict_data_mgr(self, datamgr, batchsize=None, metrics={},
@@ -420,8 +429,6 @@ if "TORCH" in get_backends():
                 filename to save the state to
             epoch : int
                 current epoch (will be saved for mapping back)
-            *args :
-                positional arguments
             **kwargs :
                 keyword arguments
 
@@ -493,6 +500,240 @@ if "TORCH" in get_backends():
 
             if "model" in new_state:
                 self.module.load_state_dict(new_state.pop("model"))
+
+            if "optimizer" in new_state and new_state["optimizer"]:
+                optim_state = new_state.pop("optimizer")
+                for key in self.optimizers.keys():
+                    self.optimizers[key].load_state_dict(
+                        optim_state[key])
+
+            if "epoch" in new_state:
+                self.start_epoch = new_state.pop("epoch")
+
+            return super()._update_state(new_state)
+
+    class PyTorchJITNetworkTrainer(PyTorchNetworkTrainer):
+        def __init__(self,
+                     network: AbstractPyTorchJITNetwork,
+                     save_path: str,
+                     key_mapping,
+                     losses=None,
+                     optimizer_cls=None,
+                     optimizer_params={},
+                     train_metrics={},
+                     val_metrics={},
+                     lr_scheduler_cls=None,
+                     lr_scheduler_params={},
+                     gpu_ids=[],
+                     save_freq=1,
+                     optim_fn=create_optims_default,
+                     logging_type="tensorboardx",
+                     logging_kwargs={},
+                     fold=0,
+                     callbacks=[],
+                     start_epoch=1,
+                     metric_keys=None,
+                     convert_batch_to_npy_fn=convert_torch_tensor_to_npy,
+                     criterions=None,
+                     val_freq=1,
+                     **kwargs):
+            """
+
+            Parameters
+            ----------
+            network : :class:`AbstractPyTorchJITNetwork`
+                the network to train
+            save_path : str
+                path to save networks to
+            key_mapping : dict
+                a dictionary containing the mapping from the ``data_dict`` to
+                the actual model's inputs.
+                E.g. if a model accepts one input named 'x' and the data_dict
+                contains one entry named 'data' this argument would have to
+                be ``{'x': 'data'}``
+            losses : dict
+                dictionary containing the training losses
+            optimizer_cls : subclass of tf.train.Optimizer
+                optimizer class implementing the optimization algorithm of
+                choice
+            optimizer_params : dict
+                keyword arguments passed to optimizer during construction
+            train_metrics : dict, optional
+                metrics, which will be evaluated during train phase
+                (should work on framework's tensor types)
+            val_metrics : dict, optional
+                metrics, which will be evaluated during test phase
+                (should work on numpy arrays)
+            lr_scheduler_cls : Any
+                learning rate schedule class: must implement step() method
+            lr_scheduler_params : dict
+                keyword arguments passed to lr scheduler during construction
+            gpu_ids : list
+                list containing ids of GPUs to use; if empty: use cpu instead
+                Currently ``torch.jit`` only supports single GPU-Training,
+                thus only the first GPU will be used if multiple GPUs are passed
+            save_freq : int
+                integer specifying how often to save the current model's state.
+                State is saved every state_freq epochs
+            optim_fn : function
+                creates a dictionary containing all necessary optimizers
+            logging_type : str or callable
+                the type of logging. If string: it must be one of
+                ["visdom", "tensorboardx"]
+                If callable: it must be a logging handler class
+            logging_kwargs : dict
+                dictionary containing all logging keyword arguments
+            fold : int
+                current cross validation fold (0 per default)
+            callbacks : list
+                initial callbacks to register
+            start_epoch : int
+                epoch to start training at
+            metric_keys : dict
+                dict specifying which batch_dict entry to use for which metric as
+                target; default: None, which will result in key "label" for all
+                metrics
+            convert_batch_to_npy_fn : type, optional
+                function converting a batch-tensor to numpy, per default this is
+                a function, which detaches the tensor, moves it to cpu and the
+                calls ``.numpy()`` on it
+            mixed_precision : bool
+                whether to use mixed precision or not (False per default)
+            mixed_precision_kwargs : dict
+                additional keyword arguments for mixed precision
+            val_freq : int
+                validation frequency specifying how often to validate the trained
+                model (a value of 1 denotes validating every epoch,
+                a value of 2 denotes validating every second epoch etc.);
+                defaults to 1
+            **kwargs :
+                additional keyword arguments
+
+            """
+
+            if len(gpu_ids) > 1:
+                # only use first GPU due to
+                # https://github.com/pytorch/pytorch/issues/15421
+                gpu_ids = [gpu_ids[0]]
+                logging.warning("Multiple GPUs specified. Torch JIT currently "
+                                "supports only single-GPU training. "
+                                "Switching to use only the first GPU for now...")
+
+            super().__init__(network=network, save_path=save_path,
+                             key_mapping=key_mapping, losses=losses,
+                             optimizer_cls=optimizer_cls,
+                             optimizer_params=optimizer_params,
+                             train_metrics=train_metrics,
+                             val_metrics=val_metrics,
+                             lr_scheduler_cls=lr_scheduler_cls,
+                             lr_scheduler_params=lr_scheduler_params,
+                             gpu_ids=gpu_ids, save_freq=save_freq,
+                             optim_fn=optim_fn, logging_type=logging_type,
+                             logging_kwargs=logging_kwargs, fold=fold,
+                             callbacks=callbacks,
+                             start_epoch=start_epoch, metric_keys=metric_keys,
+                             convert_batch_to_npy_fn=convert_batch_to_npy_fn,
+                             mixed_precision=False, mixed_precision_kwargs={},
+                             criterions=criterions, val_freq=val_freq, **kwargs
+                             )
+
+        def try_resume_training(self):
+            """
+            Load the latest state of a previous training if possible
+
+            """
+            # Load latest epoch file if available
+            if os.path.isdir(self.save_path):
+                # check all files in directory starting with "checkpoint" and
+                # not ending with "_best.pth"
+                files = [x for x in os.listdir(self.save_path)
+                         if os.path.isfile(os.path.join(self.save_path, x))
+                         and x.startswith("checkpoint")
+                         and not x.endswith("_best.ptj")
+                         ]
+
+                # if list is not empty: load previous state
+                if files:
+
+                    latest_epoch = max([
+                        int(x.rsplit("_", 1)[-1].rsplit(".", 1)[0])
+                        for x in files])
+
+                    latest_state_path = os.path.join(self.save_path,
+                                                     "checkpoint_epoch_%d.ptj"
+                                                     % latest_epoch)
+
+                    # if pth file does not exist, load pt file instead
+                    if not os.path.isfile(latest_state_path):
+                        latest_state_path = latest_state_path[:-1]
+
+                    logger.info("Attempting to load state from previous \
+                                training from %s" % latest_state_path)
+                    try:
+                        self.update_state(latest_state_path)
+                    except KeyError:
+                        logger.warning("Previous State could not be loaded, \
+                                        although it exists.Training will be \
+                                        restarted")
+
+        def save_state(self, file_name, epoch, **kwargs):
+            """
+            saves the current state via
+            :func:`delira.io.torch.save_checkpoint_jit`
+
+            Parameters
+            ----------
+            file_name : str
+                filename to save the state to
+            epoch : int
+                current epoch (will be saved for mapping back)
+            **kwargs :
+                keyword arguments
+
+            """
+            if file_name.endswith(".pt") or file_name.endswith(".pth"):
+                file_name = file_name.rsplit(".", 1)[0]
+
+            save_checkpoint_jit(file_name, self.module, self.optimizers,
+                                **kwargs)
+
+        @staticmethod
+        def load_state(file_name, **kwargs):
+            """
+            Loads the new state from file via
+            :func:`delira.io.torch.load_checkpoint:jit`
+
+            Parameters
+            ----------
+            file_name : str
+                the file to load the state from
+            **kwargs : keyword arguments
+
+            Returns
+            -------
+            dict
+                new state
+
+            """
+            return load_checkpoint_jit(file_name, **kwargs)
+
+        def _update_state(self, new_state):
+            """
+            Update the state from a given new state
+
+            Parameters
+            ----------
+            new_state : dict
+                new state to update internal state from
+
+            Returns
+            -------
+            :class:`PyTorchNetworkJITTrainer`
+                the trainer with a modified state
+
+            """
+            if "model" in new_state:
+                self.module = new_state.pop("model")
 
             if "optimizer" in new_state and new_state["optimizer"]:
                 optim_state = new_state.pop("optimizer")
