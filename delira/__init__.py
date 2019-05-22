6c3cc58563b9d8f845c9cca9e0888a6a500cd79f
Justus Schock <justus.schock@rwth-aachen.de>
2019-05-22 14:02:05 +0200

    Register SKlearn as backend in init

--- delira/__init__.py
+++ delira/__init__.py
@@ -6,13 +6,44 @@ import warnings
 warnings.simplefilter('default', DeprecationWarning)
 warnings.simplefilter('ignore', ImportWarning)
 
-# to register new pssible backends, they have to be added to this list.
+# to register new possible backends, they have to be added to this list.
 # each backend should consist of a tuple of length 2 with the first entry
 # being the package import name and the second being the backend abbreviation.
 # E.g. TensorFlow's package is named 'tensorflow' but if the package is found,
 # it will be considered as 'tf' later on
-__POSSIBLE_BACKENDS = [("torch", "torch"), ("tensorflow", "tf")]
+__POSSIBLE_BACKENDS = [("torch", "torch"), ("tensorflow", "tf"),
+                       ("sklearn", "sklearn")]
 __BACKENDS = []
+
+
+def _update_backends(config_file):
+    _backends = {}
+    # try to import all possible backends to determine valid backends
+
+    import importlib
+    for curr_backend in __POSSIBLE_BACKENDS:
+        try:
+            assert len(curr_backend) == 2
+            assert all([isinstance(_tmp, str) for _tmp in curr_backend]), \
+                "All entries in current backend must be strings"
+
+            # check if backend can be imported
+            bcknd = importlib.util.find_spec(curr_backend[0])
+
+            if bcknd is not None:
+                _backends[curr_backend[1]] = True
+            else:
+                _backends[curr_backend[1]] = False
+            del bcknd
+
+        except ValueError:
+            _backends[curr_backend[1]] = False
+
+    with open(config_file, "w") as f:
+        json.dump({"version": __version__, "backend": _backends},
+                  f, sort_keys=True, indent=4)
+
+    del _backends
 
 
 def _determine_backends():
@@ -22,33 +53,7 @@ def _determine_backends():
     # if file exists: load config into environment variables
 
     if not os.path.isfile(_config_file):
-        _backends = {}
-        # try to import all possible backends to determine valid backends
-
-        import importlib
-        for curr_backend in __POSSIBLE_BACKENDS:
-            try:
-                assert len(curr_backend) == 2
-                assert all([isinstance(_tmp, str) for _tmp in curr_backend]), \
-                    "All entries in current backend must be strings"
-
-                # check if backend can be imported
-                bcknd = importlib.util.find_spec(curr_backend[0])
-
-                if bcknd is not None:
-                    _backends[curr_backend[1]] = True
-                else:
-                    _backends[curr_backend[1]] = False
-                del bcknd
-
-            except ValueError:
-                _backends[curr_backend[1]] = False
-
-        with open(_config_file, "w") as f:
-            json.dump({"version": __version__, "backend": _backends},
-                      f, sort_keys=True, indent=4)
-
-        del _backends
+        _update_backends(_config_file)
 
     # set values from config file to variable
     with open(_config_file) as f:
@@ -61,6 +66,23 @@ def _determine_backends():
     del _config_file
 
 
+def update_backends():
+    """
+    Updates all installed backends (and deletes old backend-configuration file)
+
+    Returns
+    -------
+
+    """
+    _config_file = __file__.replace("__init__.py", ".delira")
+    _BACKENDS = []
+
+    if os.path.isfile(_config_file):
+        os.remove(_config_file)
+
+    return get_backends()
+
+
 def get_backends():
     """
     Return List of currently available backends
@@ -68,7 +90,7 @@ def get_backends():
     Returns
     -------
     list
-        list of strings containing the currently installed backends
+        list of strings indicating all currently installed backends
 
     """
 
