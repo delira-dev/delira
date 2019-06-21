d9c2e9f0f7873e7b71bf543d9c2bc1d4ec0aaa46
mibaumgartner <m.baumgartner@ymail.com>
2019-06-21 21:55:30 +0200

    Doc strings

--- delira/training/slack.py
+++ delira/training/slack.py
@@ -6,12 +6,37 @@ from delira.training.callbacks import AbstractCallback
 
 
 class SlackEpochsCallback(AbstractCallback):
-    def __init__(self, n_epochs, slack_exp):
+    def __init__(self, n_epochs: int, slack_exp):
+        """
+        Callback for "Epoch X trained" in slack
+
+        Parameters
+        ----------
+        n_epochs : int
+            notification frequency
+        slack_exp : :class:`SlackExperiment`
+            instance of a slack experiment to emit slack message
+        """
         super().__init__()
         self._n_epochs = n_epochs
         self._slack_exp = slack_exp
 
-    def at_epoch_end(self, trainer, **kwargs):
+    def at_epoch_end(self, trainer, **kwargs) -> dict:
+        """
+        Call at end of epoch
+
+        Parameters
+        ----------
+        trainer : :class:`BaseTrainer`
+            instance of trainer
+        kwargs :
+            additional keyword arguments. Must contain ``curr_epoch``.
+
+        Returns
+        -------
+        dict
+            empty dict
+        """
         curr_epoch = kwargs.pop("curr_epoch")
         trained_epochs = curr_epoch - trainer.start_epoch
         if trained_epochs % self._n_epochs == 0:
@@ -22,10 +47,33 @@ class SlackEpochsCallback(AbstractCallback):
 
 class SlackFoldCallback(AbstractCallback):
     def __init__(self, slack_exp):
+        """
+        Callback for "Fold X completed" in slack
+
+        Parameters
+        ----------
+        slack_exp : :class:`SlackExperiment`
+            instance of a slack experiment to emit slack message
+        """
         super().__init__()
         self._slack_exp = slack_exp
 
-    def at_training_end(self, trainer, **kwargs):
+    def at_training_end(self, trainer, **kwargs) -> dict:
+        """
+        End of training callback
+
+        Parameters
+        ----------
+        trainer : :class:`BaseTrainer`
+            instance of trainer
+        kwargs :
+            additional keyword arguments (not used)
+
+        Returns
+        -------
+        dict
+            empty dict
+        """
         msg = "Fold " + str(trainer.fold) + " completed."
         self._slack_exp.emit_message(msg)
         return {}
@@ -34,6 +82,25 @@ class SlackFoldCallback(AbstractCallback):
 class SlackExperiment(object):
     def __init__(self, experiment: BaseExperiment, token: str,
                  channel: str, *args, notify_epochs=None, **kwargs):
+        """
+        Wrap arbitrary experiments and connect its functions to slack
+        notification
+
+        Parameters
+        ----------
+        experiment : :class:`BaseExperiment`
+            instance of current experiment
+        token : str
+            User or Bot token from slack
+        channel : str
+            channel id (destination of messages)
+        args :
+            additional positional arguments passed to :class:`SlackClient`
+        notify_epochs : int
+            Activates additional notifications with frequency `notify_epochs`.
+        kwargs :
+            additional keyword arguments passed to :class:`SlackClient`
+        """
         # wrap the object
         self._client = SlackClient(token, *args, **kwargs)
         self._channel = channel
@@ -51,6 +118,20 @@ class SlackExperiment(object):
         self._ts = resp['ts']
 
     def __getattr__(self, attr):
+        """
+        If wrapper does not implement attribute, return attribute of wrapped
+        object
+
+        Parameters
+        ----------
+        attr : str
+            name of attribute
+
+        Returns
+        -------
+        Any
+            attribute
+        """
         # see if this object has attr
         # NOTE do not use hasattr, it goes into
         # infinite recurrsion
@@ -60,7 +141,20 @@ class SlackExperiment(object):
         # proxy to the wrapped object
         return getattr(self._wrapped_exp, attr)
 
-    def emit_message(self, msg):
+    def emit_message(self, msg) -> dict:
+        """
+        Emit message to current experiment thread
+
+        Parameters
+        ----------
+        msg : str
+            message which should be emitted
+
+        Returns
+        -------
+        dict
+            dict with additional information from message
+        """
         resp = self._client.api_call(
             "chat.postMessage",
             channel=self._channel,
@@ -73,6 +167,21 @@ class SlackExperiment(object):
         return resp
 
     def run(self, *args, **kwargs):
+        """
+        Wrapper for run function. Notifies experiment start, fail, complete.
+
+        Parameters
+        ----------
+        args :
+            positional arguments passed to experiment.
+        kwargs :
+            additional keyword arguments passed to experiment.
+
+        Returns
+        -------
+        Any
+            result of experiment
+        """
         if self._notify_epochs is not None:
             callbacks = kwargs.pop("callbacks", [])
             callbacks.append(SlackEpochsCallback(self._notify_epochs,
@@ -95,6 +204,21 @@ class SlackExperiment(object):
         return out
 
     def resume(self, *args, **kwargs):
+        """
+        Wrapper for resume function. Notifies experiment start, fail, complete.
+
+        Parameters
+        ----------
+        args :
+            positional arguments passed to experiment.
+        kwargs :
+            additional keyword arguments passed to experiment.
+
+        Returns
+        -------
+        Any
+            result of experiment
+        """
         if self._notify_epochs is not None:
             callbacks = kwargs.pop("callbacks", [])
             callbacks.append(SlackEpochsCallback(self._notify_epochs,
@@ -116,6 +240,21 @@ class SlackExperiment(object):
         return out
 
     def test(self, *args, **kwargs):
+        """
+        Wrapper for test function. Notifies experiment start, fail, complete.
+
+        Parameters
+        ----------
+        args :
+            positional arguments passed to experiment.
+        kwargs :
+            additional keyword arguments passed to experiment.
+
+        Returns
+        -------
+        Any
+            result of experiment
+        """
         msg = str(self._wrapped_exp.name) + " : Test completed."
         self.emit_message(msg)
 
@@ -131,6 +270,22 @@ class SlackExperiment(object):
         return out
 
     def kfold(self, *args, **kwargs):
+        """
+        Wrapper for kfold function. Notifies experiment start, fail, complete,
+        end of fold.
+
+        Parameters
+        ----------
+        args :
+            positional arguments passed to experiment.
+        kwargs :
+            additional keyword arguments passed to experiment.
+
+        Returns
+        -------
+        Any
+            result of experiment
+        """
         callbacks = kwargs.pop("callbacks", [])
         callbacks.append(SlackFoldCallback(self))
 
