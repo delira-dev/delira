ef5e80a09ba90641fd3d2daf3112f3376e1fbb73
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-06-25 09:38:43 +0200

    Update imports

--- delira/data_loading/sampler/__init__.py
+++ delira/data_loading/sampler/__init__.py
@@ -1,20 +1,10 @@
-from .abstract_sampler import AbstractSampler
-from .lambda_sampler import LambdaSampler
-from .random_sampler import RandomSampler, PrevalenceRandomSampler, \
-    StoppingPrevalenceRandomSampler
-from .sequential_sampler import SequentialSampler, \
-    PrevalenceSequentialSampler, StoppingPrevalenceSequentialSampler
-from .weighted_sampler import WeightedRandomSampler, \
-    WeightedPrevalenceRandomSampler
-
-__all__ = [
-    'AbstractSampler',
-    'SequentialSampler',
-    'PrevalenceSequentialSampler',
-    'StoppingPrevalenceSequentialSampler',
-    'RandomSampler',
-    'PrevalenceRandomSampler',
-    'StoppingPrevalenceRandomSampler',
-    'WeightedRandomSampler',
-    'LambdaSampler'
-]
+from delira.data_laoding.sampler.abstract_sampler import AbstractSampler
+from delira.data_laoding.sampler.lambda_sampler import LambdaSampler
+from delira.data_laoding.sampler.random_sampler import RandomSampler, \
+    PerClassRandomSampler, StoppingPerClassRandomSampler
+from delira.data_laoding.sampler.sequential_sampler import \
+    SequentialSampler, PerClassSequentialSampler, \
+    StoppingPerClassSequentialSampler
+from delira.data_laoding.sampler.weighted_sampler import \
+    WeightedRandomSampler, WeightedPrevalenceRandomSampler
+from delira.data_laoding.sampler.batch_sampler import BatchSampler
