a9c5e986d85244fbf71c84f0c9e615e1ad5f364e
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-06-25 09:37:14 +0200

    Update Weighted Sampler to single sample

--- delira/data_loading/sampler/weighted_sampler.py
+++ delira/data_loading/sampler/weighted_sampler.py
@@ -1,19 +1,17 @@
 import numpy as np
 from numpy.random import choice
 
-from .abstract_sampler import AbstractSampler
-from ..dataset import AbstractDataset
+from delira.data_loading.sampler.abstract_sampler import AbstractSampler
+from delira.data_loading.dataset import AbstractDataset
 
 
 class WeightedRandomSampler(AbstractSampler):
     """
     Implements Weighted Random Sampling
-
     """
 
     def __init__(self, indices, weights=None):
         """
-
         Parameters
         ----------
         indices : list
@@ -23,7 +21,6 @@ class WeightedRandomSampler(AbstractSampler):
         weights : Any or None
             sampling weights; for more details see numpy.random.choice
             (parameter ``p``)
-
         """
         super().__init__(indices)
 
@@ -31,56 +28,24 @@ class WeightedRandomSampler(AbstractSampler):
         self._weights = weights
         self._global_index = 0
 
+    def _get_next_index(self):
+        return choice(self._indices, p=self._weights)
+
     @classmethod
     def from_dataset(cls, dataset: AbstractDataset, **kwargs):
         """
-
         Classmethod to initialize the sampler from a given dataset
-
         Parameters
         ----------
         dataset : AbstractDataset
             the given dataset
-
         Returns
         -------
         AbstractSampler
             The initialzed sampler
-
         """
         labels = [d['label'] for d in dataset]
         return cls(labels, **kwargs)
-
-    def _get_indices(self, n_indices):
-        """
-        Actual Sampling
-
-        Parameters
-        ----------
-        n_indices : int
-            number of indices to return
-
-        Returns
-        -------
-        list
-            list of sampled indices
-
-        Raises
-        ------
-        StopIteration
-            If maximal number of samples is reached
-        ValueError
-            if weights or cum_weights don't match the population
-
-        """
-        n_indices = self._check_batchsize(n_indices)
-
-        samples = choice(self._indices, size=n_indices, p=self._weights)
-
-        return samples
-
-    def __len__(self):
-        return len(self._indices)
 
 
 class WeightedPrevalenceRandomSampler(WeightedRandomSampler):
@@ -88,7 +53,6 @@ class WeightedPrevalenceRandomSampler(WeightedRandomSampler):
         """
         Implements random Per-Class Sampling and ensures uniform sampling
         of all classes
-
         Parameters
         ----------
         indices : array-like
