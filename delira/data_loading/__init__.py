5835095c7123c34e20223d322dd70236c3f1393e
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-06-25 09:46:35 +0200

    Update imports

--- delira/data_loading/__init__.py
+++ delira/data_loading/__init__.py
@@ -1,21 +1,24 @@
 
-from delira import get_backends
-from .data_loader import BaseDataLoader
-from .data_manager import BaseDataManager
-from .dataset import AbstractDataset, BaseCacheDataset, BaseLazyDataset, \
-    ConcatDataset, BaseExtendCacheDataset
-from .load_utils import default_load_fn_2d, LoadSample, LoadSampleLabel
-from .sampler import LambdaSampler, \
-    WeightedRandomSampler, \
-    PrevalenceRandomSampler, \
-    RandomSampler, \
-    StoppingPrevalenceSequentialSampler, \
-    SequentialSampler
+# basic imports
+from delira.data_loading.data_loader import DataLoader
+from delira.data_loading.dataset import AbstractDataset, IterableDataset, \
+    DictDataset, BaseCacheDataset, BaseExtendCacheDataset, BaseLazyDataset, \
+    ConcatDataset, Nii3DCacheDatset, Nii3DLazyDataset
+from delira.data_loading.augmenter import Augmenter
+from delira.data_loading.data_manager import DataManager
+from delira.data_loading.load_utils import  LoadSample, LoadSampleLabel
 
-if "TORCH" in get_backends():
-    from .dataset import TorchvisionClassificationDataset
+from delira.data_loading.sampler import *
 
 
+from delira import get_backends as _get_backends
+
+# If torch backend is available: Import Torchvision dataset
+if "TORCH" in _get_backends():
+    from delira.data_loading.dataset import TorchvisionClassificationDataset
+
+
+# if numba is installed: Import Numba Transforms
 try:
     from delira.data_loading.numba_transform import NumbaTransform, \
         NumbaTransformWrapper, NumbaCompose
