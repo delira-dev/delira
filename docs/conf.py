5cd7024c272659569f0999877da33acd8092effb
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-07-29 16:15:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,15 +12,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-
-from delira._version import get_versions
 import os
 import sys
 import re
 
 # source code directory, relative to this file, for sphinx-build
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.path.pardir))
-
+from delira._version import get_versions
 
 # -- Project information -----------------------------------------------------
 
