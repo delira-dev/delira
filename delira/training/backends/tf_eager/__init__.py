fe977b71ce43f94c9e1bf4517a0eeb779f8b616b
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-29 13:08:24 +0200

    Remove broken import

--- delira/training/backends/tf_eager/__init__.py
+++ delira/training/backends/tf_eager/__init__.py
@@ -5,7 +5,5 @@ if "TF" in _get_backends():
     from delira.training.backends.tf_eager.trainer import TfEagerNetworkTrainer
     from delira.training.backends.tf_eager.utils import convert_to_numpy \
         as convert_tfeager_to_numpy
-    from delira.training.backends.tf_eager.utils import \
-        switch_tf_execution_mode
     from delira.training.backends.tf_eager.utils import create_optims_default \
         as create_tfeager_optims_default
