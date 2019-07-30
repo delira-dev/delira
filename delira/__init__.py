1d843cbf91cc9a31ce7a7811e5ed34a82ab0c55f
Justus Schock <justus.schock@rwth-aachen.de>
2019-07-30 16:15:59 +0200

    Add Import

--- delira/__init__.py
+++ delira/__init__.py
@@ -1,6 +1,6 @@
 from delira._debug_mode import get_current_debug_mode, switch_debug_mode, \
     set_debug_mode
-from delira._backends import get_backends
+from delira._backends import get_backends, seed_all
 
 from ._version import get_versions
 import json
