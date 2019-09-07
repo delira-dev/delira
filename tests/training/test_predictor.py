439a61a42d6e4cc76c4b1e79fa4c9e4030cf30a9
Justus Schock <justus.schock@rwth-aachen.de>
2019-09-07 09:59:11 +0200

    Add additional TTA Test

--- tests/training/test_predictor.py
+++ tests/training/test_predictor.py
@@ -1,9 +1,21 @@
-from tests.utils import check_for_no_backend
+from ..utils import check_for_no_backend
 from delira.training import Predictor
 import unittest
 import numpy as np
 from batchgenerators.transforms import RangeTransform, \
-    ContrastAugmentationTransform
+    ContrastAugmentationTransform, AbstractTransform
+
+
+class MakeValueTrafo(AbstractTransform):
+    def __init__(self, value):
+        self._value = value
+
+    def __call__(self, **data_dict):
+        for k, v in data_dict.items():
+            v = np.ones_like(v) * self._value
+            data_dict[k] = v
+
+        return data_dict
 
 
 def network_dummy_fn(x):
@@ -18,7 +30,7 @@ class TestPredictor(unittest.TestCase):
     @unittest.skipUnless(
         check_for_no_backend(),
         "Test should only be executed if no backend is installed")
-    def test_tta(self):
+    def test_tta_one_trafo(self):
 
         self._predictor = Predictor(model=network_dummy_fn,
                                     key_mapping={"x": "data"},
@@ -35,3 +47,28 @@ class TestPredictor(unittest.TestCase):
         self.assertTupleEqual(preds["pred"].shape, (3, 4, 3, 32, 32))
         self.assertFalse(all((preds["pred"] == _pred).all()
                              for _pred in preds["pred"]))
+
+    def test_tta_back_trafo(self):
+        self._predictor = Predictor(model=network_dummy_fn,
+                                    key_mapping={"x": "data"},
+                                    tta_transforms=(
+                                        MakeValueTrafo(1.),
+                                    ),
+                                    tta_inverse_transforms=(
+                                        MakeValueTrafo(0.),
+                                    ),
+                                    tta_reduce_fn=dummy_reduce_fn)
+
+        preds = self._predictor({"data": np.random.rand(4, 3, 32, 32)})
+
+        # check for shape (1 per transform + original)
+        self.assertTupleEqual(preds["pred"].shape, (2, 4, 3, 32, 32))
+
+        # Check if all values are zero (which means,
+        # the inverse trafo worked correctly)
+        self.assertFalse(preds["pred"][1:].any())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
